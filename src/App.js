6fc814643e22735ad792024518ec2633968581f5
Khadijah-Developer <88772180+Khadijah-Developer@users.noreply.github.com>
2022-08-24 11:58:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,46 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import Example from './components/Example';
 
-// function App() {
-//     function tossCoin() {
-//         return Math.random() > 0.5 ? "heads" : "tails";
-//     }
+function App() {
+    function tossCoin() {
+        return Math.random() > 0.5 ? "heads" : "tails";
+    }
   
-//     function fiveHeads() {
-//       return new Promise( (resolve, reject) => {
-//           // your code here!
-//           let headsCount = 0;
-//           let attempts = 0;
-//           while(headsCount < 5) {
-//             attempts++;
-//             let result = tossCoin();
-//             //console.log(`${result} was flipped`);
-//             if(result === "heads") {
-//                 headsCount++;
-//             } else {
-//                 headsCount = 0;
-//             }
+    function fiveHeads() {
+      return new Promise( (resolve, reject) => {
+          // your code here!
+          let headsCount = 0;
+          let attempts = 0;
+          while(headsCount < 5) {
+            attempts++;
+            let result = tossCoin();
+            //console.log(`${result} was flipped`);
+            if(result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
 
-//         }
+        }
 
 
-//         if(headsCount === 5 && attempts < 100) {
-//           resolve(`Success! It took ${attempts} tries to flip 5 heads in a row`);
-//         } else {
-//           reject(`Did not complete in less than 100 attempts! It took ${attempts} tries to flip 5 `)
-//         }
-//       });
-//   }
-//   fiveHeads()
-//       .then( res => console.log(res) )
-//       .catch( err => console.log(err) );
-//   console.log( "When does this run now?" );
-//   return (
-//     <div className="App">
-//    <h1> coin flipping</h1>
-//     </div>
-//   );
-// }
+        if(headsCount === 5 && attempts < 100) {
+          resolve(`Success! It took ${attempts} tries to flip 5 heads in a row`);
+        } else {
+          reject(`Did not complete in less than 100 attempts! It took ${attempts} tries to flip 5 `)
+        }
+      });
+  }
+  fiveHeads()
+      .then( res => console.log(res) )
+      .catch( err => console.log(err) );
+  console.log( "When does this run now?" );
+  return (
+    <div className="App">
+   <h1> coin flipping</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
